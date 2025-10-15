82e84748424d6f794bc6ecaaf92c4eba365cebd5
GeetanshJ <jaingeetansh@gmail.com>
2025-10-15 20:39:34 +0530

    first

---
+++ Finance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Project
+{
+    public class User
+    {
+
+    }
+
+    class Admin : User
+    {
+
+    }
+
+    class UnderWriter : User
+    {
+
+    }
+
+    class Vehicle
+    {
+
+    }
+}
+
+class Hello
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
